a23ac60b8a25bf79ffe2b6b96ac87f632ba33cd8
tielenn <mcore.1212@gmail.com>
2020-07-01 10:59:31 +0300

    ended part 4, final

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,41 +2,35 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../../util/Yelp';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-]
-
-function App() {
-
-  function searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      businesses: []
+    }
+    this.searchYelp = this.searchYelp.bind(this)
   }
 
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar searchYelp={searchYelp} />
-      <BusinessList businesses={businesses} />
-    </div>
-  );
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({
+        businesses: businesses
+      })
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+      )
+  }
 }
 
 export default App;
---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = 'itI5zwpwspQPLWW5xEQE6RRWwyeEcpvbjsnfoWc3RcLqzkMvIzPXeodOHfCLyiBzJXc0jV_rwZx3rUiyejJPED23YzsuuHktCk2V6J9DfWOGur962TYBwlsaxzP7XnYx';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+            }).then(response => response.json())
+            .then(jsonResponse => {
+                if (jsonResponse.businesses) {
+                    return jsonResponse.businesses.map(business => {
+                        
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            categoty: business.categories[0].title,
+                            rating: business.rating,
+                            reviewCount: business.review_count
+                        }
+                    })
+                } 
+            })
+    }
+};
+
+export default Yelp;
